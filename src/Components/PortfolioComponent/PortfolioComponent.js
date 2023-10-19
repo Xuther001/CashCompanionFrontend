7d1a6910d526a023e20d8fd90a009f110d48f7c6
Will <wt@Wills-MacBook-Pro.local>
2023-10-19 14:07:44 -0700

    added a net gain or loss field in PortfolioComponent

--- src/Components/PortfolioComponent/PortfolioComponent.js
+++ src/Components/PortfolioComponent/PortfolioComponent.js
@@ -20,19 +20,38 @@ function PortfolioComponent() {
     }
   };
 
-  const addInvestment = () => {
-    setInvestments([...investments, newInvestment]);
-    setNewInvestment({
-      name: '',
-      shares: 0,
-      purchasePrice: "$",
-    });
+  const addInvestment = async () => {
+    // Fetch the current stock price from the API
+    try {
+      const response = await fetch(`https://finnhub.io/api/v1/quote?symbol=${newInvestment.name}&token=ckifd3hr01qi7b5gm7dgckifd3hr01qi7b5gm7e0`);
+      const data = await response.json();
+      
+      // Calculate the net loss or gain
+      const currentPrice = data.c;
+      const purchasePrice = parseFloat(newInvestment.purchasePrice.replace('$', ''));
+      const shares = parseFloat(newInvestment.shares);
+      const netGainOrLoss = (currentPrice - purchasePrice) * shares;
+
+      // Update investments state with the new investment and net gain/loss
+      setInvestments([...investments, { ...newInvestment, currentPrice, netGainOrLoss }]);
+      setNewInvestment({
+        name: '',
+        shares: 0,
+        purchasePrice: "$",
+      });
+    } catch (error) {
+      console.error('Error fetching data from API:', error);
+    }
   };
 
   const investmentList = investments.map((investment, index) => (
     <div key={index}>
       <p>
-        Stock Symbol: {investment.name} || Shares: {investment.shares} || Purchase Price: ${parseFloat(investment.purchasePrice.replace('$', '')).toLocaleString('en-US')}
+        Stock Symbol: {investment.name} <br />
+        Shares: {investment.shares} <br />
+        Purchase Price: {investment.purchasePrice} <br />
+        Current Price: {investment.currentPrice} <br />
+        Net Gain/Loss: ${investment.netGainOrLoss.toFixed(2)}
       </p>
     </div>
   ));
@@ -42,32 +61,38 @@ function PortfolioComponent() {
       <h2>Investment Tracker</h2>
       <div className="add-investment-container">
         <h3>Add New Investment</h3>
-        <label className="add-investment-label" htmlFor="name">Stock Symbol:</label>
-        <input
-          className="add-investment-input"
-          type="text"
-          name="name"
-          value={newInvestment.name}
-          onChange={handleInputChange}
-        />
+        <div className="input-group">
+          <label className="add-investment-label" htmlFor="name">Stock Symbol:</label>
+          <input
+            className="add-investment-input"
+            type="text"
+            name="name"
+            value={newInvestment.name}
+            onChange={handleInputChange}
+          />
+        </div>
 
-        <label className="add-investment-label" htmlFor="shares">Shares:</label>
-        <input
-          className="add-investment-input"
-          type="number"
-          name="shares"
-          value={newInvestment.shares}
-          onChange={handleInputChange}
-        />
+        <div className="input-group">
+          <label className="add-investment-label" htmlFor="shares">Shares:</label>
+          <input
+            className="add-investment-input"
+            type="number"
+            name="shares"
+            value={newInvestment.shares}
+            onChange={handleInputChange}
+          />
+        </div>
 
-        <label className="add-investment-label" htmlFor="purchasePrice">Purchase Price:</label>
-        <input
-          className="add-investment-input"
-          type="string"
-          name="purchasePrice"
-          value={newInvestment.purchasePrice}
-          onChange={handleInputChange}
-        />
+        <div className="input-group">
+          <label className="add-investment-label" htmlFor="purchasePrice">Purchase Price Per Share:</label>
+          <input
+            className="add-investment-input"
+            type="string"
+            name="purchasePrice"
+            value={newInvestment.purchasePrice}
+            onChange={handleInputChange}
+          />
+        </div>
 
         <div style={{ textAlign: 'center' }}>
           <button className="add-investment-button" onClick={addInvestment}>Add Investment</button>
